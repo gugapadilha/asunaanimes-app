08cf3603a1e46b932f84b0ad2b09d0bf8cbea5a7
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-05-17 17:26:07 -0300

    added animated border card animation

--- app/src/main/java/com/example/myanimelist/InitialScreen.kt
+++ app/src/main/java/com/example/myanimelist/InitialScreen.kt
@@ -1,17 +1,37 @@
 package com.example.myanimelist
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 
@@ -28,15 +48,70 @@ fun InitialScreen() {
             contentDescription = "Anime Screen",
             contentScale = ContentScale.FillBounds
         )
-
-        Column(
-            modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 70.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        AnimatedBorderCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 40.dp),
+            shape = RoundedCornerShape(8.dp),
+            borderWidth = 1.dp,
+            gradient = Brush.linearGradient(listOf(Color.Magenta, Color.Cyan)),
+            onCardClick = {}
         ) {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Get started")
-            }
+            Text(
+                text = "Get started",
+                color = Color.White,
+            )
+        }
+    }
+
+}
+
+@Composable
+fun AnimatedBorderCard(
+    modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(size = 0.dp),
+    borderWidth: Dp = 2.dp,
+    gradient: Brush = Brush.sweepGradient(listOf(Color.Gray, Color.White)),
+    animationDuration: Int = 10000,
+    onCardClick: () -> Unit = {},
+    content: @Composable () -> Unit,
+
+    ) {
+    val infiniteTransition = rememberInfiniteTransition(label = "Infinite Color Animation")
+    val degrees by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = animationDuration, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "Infinite Colors"
+    )
+
+    Surface(
+        modifier = modifier
+            .clip(shape)
+            .clickable { onCardClick() },
+        shape = shape
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(borderWidth)
+                .drawWithContent {
+                    rotate(degrees = degrees) {
+                        drawCircle(
+                            brush = gradient,
+                            radius = size.width,
+                            blendMode = BlendMode.SrcIn,
+                        )
+                    }
+                    drawContent()
+                },
+            color = Color.Black,
+            shape = shape
+        ) {
+            content()
         }
     }
 }
