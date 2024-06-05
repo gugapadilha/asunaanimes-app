8b6c5db334509a57e22fbc0a17d50aae9df0ca3d
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-05 13:43:23 -0300

    adding anime photo to home screen

--- app/src/main/java/com/example/myanimelist/ui/HomeScreen.kt
+++ app/src/main/java/com/example/myanimelist/ui/HomeScreen.kt
@@ -1,11 +1,31 @@
 package com.example.myanimelist.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import com.example.myanimelist.R
 
 @Composable
 fun HomeScreen(navController: NavController) {
     
-    Text(text = "Hello")
+    val painter = rememberAsyncImagePainter(R.drawable.home_screen)
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ){
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painter,
+            contentDescription = "Anime Screen",
+            contentScale = ContentScale.FillBounds
+        )
+    }
 }
