5286e48181b82a7ba90e323d0c3b6bfb5cbe90ff
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-05 13:50:08 -0300

    working on cards

--- app/src/main/java/com/example/myanimelist/ui/HomeScreen.kt
+++ app/src/main/java/com/example/myanimelist/ui/HomeScreen.kt
@@ -1,31 +1,78 @@
 package com.example.myanimelist.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.example.myanimelist.R
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    
+
     val painter = rememberAsyncImagePainter(R.drawable.home_screen)
 
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.BottomCenter
-    ){
+    ) {
         Image(
             modifier = Modifier.fillMaxSize(),
             painter = painter,
             contentDescription = "Anime Screen",
             contentScale = ContentScale.FillBounds
         )
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp, 100.dp, 0.dp, 0.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AnimatedBorderCard(
+            modifier = Modifier
+                .width(130.dp)
+                .height(90.dp)
+                .padding(bottom = 40.dp),
+            shape = RoundedCornerShape(24.dp),
+            borderWidth = 3.dp,
+            gradient = Brush.linearGradient(
+                listOf(
+                    Color(android.graphics.Color.rgb(117, 27, 16)),
+                    Color.Gray
+                )
+            ),
+            onCardClick = {}
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier.clickable {
+                        navController.navigate(route = "watched_screen")
+                    },
+                    text = "Get started",
+                    color = Color.White,
+                )
+            }
+        }
     }
 }
