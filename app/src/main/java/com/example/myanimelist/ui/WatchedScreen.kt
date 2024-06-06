67bb0f8ec8740078620359230a14874a652e66e8
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-06 15:40:39 -0300

    images being showing as expected

--- app/src/main/java/com/example/myanimelist/ui/WatchedScreen.kt
+++ app/src/main/java/com/example/myanimelist/ui/WatchedScreen.kt
@@ -1,12 +1,18 @@
 package com.example.myanimelist.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
 import com.example.myanimelist.R
@@ -14,8 +20,7 @@ import com.example.myanimelist.data.repository.AnimeRepository
 import com.example.myanimelist.ui.util.CustomItem
 
 @Composable
-fun WatchedScreen(navController : NavHostController) {
-
+fun WatchedScreen(navController: NavHostController) {
     val painter = rememberAsyncImagePainter(R.drawable.watched_screen)
 
     val animeRepository = AnimeRepository()
@@ -28,13 +33,17 @@ fun WatchedScreen(navController : NavHostController) {
         Image(
             modifier = Modifier.fillMaxSize(),
             painter = painter,
-            contentDescription = "Anime Screen",
+            contentDescription = "Background Image",
             contentScale = ContentScale.FillBounds
         )
 
-        animeList.forEach { anime ->
-            CustomItem(anime = anime)
+        Row(
+            modifier = Modifier.horizontalScroll(rememberScrollState()),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            animeList.forEach { anime ->
+                CustomItem(anime = anime)
+            }
         }
     }
-
 }
--- app/src/main/java/com/example/myanimelist/ui/util/CustomItem.kt
+++ app/src/main/java/com/example/myanimelist/ui/util/CustomItem.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,7 +30,9 @@ fun CustomItem(anime: Anime) {
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         Image(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .width(120.dp)
+                .height(120.dp),
             painter = rememberAsyncImagePainter(anime.photoUrl),
             contentDescription = "Anime Screen",
             contentScale = ContentScale.FillBounds
