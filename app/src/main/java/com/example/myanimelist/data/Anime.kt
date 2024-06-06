25a5164e68eca06e9e6bf9a39b014771324209f5
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-06 14:59:55 -0300

    create anime data class

---
+++ app/src/main/java/com/example/myanimelist/data/Anime.kt
@@ -0,0 +1,5 @@
+package com.example.myanimelist.data
+
+data class Anime(
+    val photoUrl: String
+)
--- app/src/main/java/com/example/myanimelist/ui/WatchedScreen.kt
+++ app/src/main/java/com/example/myanimelist/ui/WatchedScreen.kt
@@ -1,10 +1,31 @@
 package com.example.myanimelist.ui
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.navigation.NavHostController
+import coil.compose.rememberAsyncImagePainter
+import com.example.myanimelist.R
 
 @Composable
 fun WatchedScreen(navController : NavHostController) {
-    Text(text = "WATCHED SCREEN!")
+
+    val painter = rememberAsyncImagePainter(R.drawable.watched_screen)
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painter,
+            contentDescription = "Anime Screen",
+            contentScale = ContentScale.FillBounds
+        )
+    }
+
 }
