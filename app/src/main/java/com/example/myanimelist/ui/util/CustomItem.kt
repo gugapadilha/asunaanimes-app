cbed4647f2edfa4ceb1ea7416123e45796eafb08
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-10 14:48:11 -0300

    done text to inform data

--- app/src/main/java/com/example/myanimelist/ui/util/CustomItem.kt
+++ app/src/main/java/com/example/myanimelist/ui/util/CustomItem.kt
@@ -18,7 +18,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.example.myanimelist.data.model.Anime
 import com.example.myanimelist.data.model.Year
@@ -54,6 +56,11 @@ fun CustomData() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text( text = "2024", color = Color.White)
+        Text(
+            text = "2024",
+            color = Color.White,
+            fontSize = 28.sp,
+            fontFamily = FontFamily.SansSerif
+        )
     }
 }
