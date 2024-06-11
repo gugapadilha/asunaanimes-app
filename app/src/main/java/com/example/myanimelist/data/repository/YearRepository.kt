0e3728c485cdf523cc7644c3478f4a2edb560a50
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-11 11:22:04 -0300

    created fun to fill all years dates

--- app/src/main/java/com/example/myanimelist/data/repository/YearRepository.kt
+++ app/src/main/java/com/example/myanimelist/data/repository/YearRepository.kt
@@ -1,4 +1,26 @@
 package com.example.myanimelist.data.repository
 
+import com.example.myanimelist.data.model.Year
+
 class YearRepository {
+
+    fun getAllYears(): List<Year>{
+        return listOf(
+            Year(
+                2024
+            ),
+            Year(
+                2023
+            ),
+            Year(
+                2022
+            ),
+            Year(
+                2021
+            ),
+            Year(
+                2020
+            ),
+        )
+    }
 }
