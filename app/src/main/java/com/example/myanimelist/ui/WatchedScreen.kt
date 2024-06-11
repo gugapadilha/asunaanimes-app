5493f767ac9a91fa1e78c738e3f0c2a28d582b41
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-11 11:58:54 -0300

    images being showed as expected

--- app/src/main/java/com/example/myanimelist/ui/WatchedScreen.kt
+++ app/src/main/java/com/example/myanimelist/ui/WatchedScreen.kt
@@ -14,16 +14,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
 import com.example.myanimelist.R
 import com.example.myanimelist.data.repository.AnimeRepository
+import com.example.myanimelist.data.repository.YearRepository
 import com.example.myanimelist.ui.util.CustomData
 import com.example.myanimelist.ui.util.CustomItem
 
@@ -33,6 +37,10 @@ fun WatchedScreen(navController: NavHostController) {
 
     val animeRepository = AnimeRepository()
     val animeList = animeRepository.getAllData()
+
+    val yearRepository = YearRepository()
+    val yearList = yearRepository.getAllYears()
+
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -44,29 +52,37 @@ fun WatchedScreen(navController: NavHostController) {
             contentDescription = "Background Image",
             contentScale = ContentScale.FillBounds
         )
-
         Column(
             modifier = Modifier
                 .fillMaxSize(),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            CustomData()
-            Row(
-                modifier = Modifier.horizontalScroll(rememberScrollState()),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                animeList.forEach { anime ->
-                    CustomItem(anime = anime)
+            yearList.forEach { year ->
+                Text(
+                    text = year.year.toString(),
+                    color = Color.White,
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 28.sp,
+                    modifier = Modifier.padding(top = 12.dp)
+                )
+                val animeList = animeRepository.getAllData() // Obter lista de animes para o ano atual
+                Row(
+                    modifier = Modifier.horizontalScroll(rememberScrollState()),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    animeList.forEach { anime ->
+                        CustomItem(anime = anime)
+                    }
                 }
+                Spacer(
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .fillMaxWidth()
+                        .height(1.dp)
+                        .background(Color.White)
+                )
             }
-            Spacer(
-                modifier = Modifier
-                    .padding(top = 10.dp)
-                    .fillMaxWidth()
-                    .height(1.dp)
-                    .background(Color.White)
-            )
         }
     }
 }
