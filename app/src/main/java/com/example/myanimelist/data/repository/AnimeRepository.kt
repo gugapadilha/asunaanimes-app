08e9fc4bf9f8f127ff7bc6d314034b5ea5f18fdc
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-11 12:31:59 -0300

    added new animes to list

--- app/src/main/java/com/example/myanimelist/data/repository/AnimeRepository.kt
+++ app/src/main/java/com/example/myanimelist/data/repository/AnimeRepository.kt
@@ -10,22 +10,34 @@ class AnimeRepository {
                 photoUrl = "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/fdc0dff409f19dfd8ffff5037257ac98.jpe"
             ),
             Anime(
-                photoUrl = "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/fdc0dff409f19dfd8ffff5037257ac98.jpe"
+                photoUrl = "https://m.media-amazon.com/images/S/pv-target-images/f6229414d529451c2677a18db912a09fc8ee78a78669cf7043acbc8d952a6f8c.jpg"
             ),
             Anime(
-                photoUrl = "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/fdc0dff409f19dfd8ffff5037257ac98.jpe"
+                photoUrl = "https://puui.wetvinfo.com/vcover_hz_pic/0/dk9dwf1131racnz1615443548288/0"
             ),
             Anime(
-                photoUrl = "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/fdc0dff409f19dfd8ffff5037257ac98.jpe"
+                photoUrl = "https://www.mangamag.fr/wp-content/uploads/2022/02/Liste-episodes-demon-slayer-1.png.jpg"
             ),
             Anime(
-                photoUrl = "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/fdc0dff409f19dfd8ffff5037257ac98.jpe"
+                photoUrl = "https://meups.com.br/wp-content/uploads/2022/08/Cyberpunk.jpg"
             ),
             Anime(
-                photoUrl = "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/fdc0dff409f19dfd8ffff5037257ac98.jpe"
+                photoUrl = "https://m.media-amazon.com/images/S/pv-target-images/cc1a5f842ea4e49fadc4db30a374fd23586fdfbd8f921cfdbb7ddcc51afdc064.jpg"
             ),
             Anime(
-                photoUrl = "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/fdc0dff409f19dfd8ffff5037257ac98.jpe"
+                photoUrl = "https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/aa3f7282fe345e81a1addada43bfad92.jpe"
+            ),
+            Anime(
+                photoUrl = "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/03/konosuba-season-3-feature.jpg"
+            ),
+            Anime(
+                photoUrl = "https://static.wikia.nocookie.net/liberproeliis/images/b/bd/Akame_ga_kill_by_marcus_sen-d8a239n.png/revision/latest/scale-to-width-down/680?cb=20161211100737&path-prefix=pt-br"
+            ),
+            Anime(
+                photoUrl = "https://dimensaosete.com.br/static/97d33cbc5d14ee7035a9843d05c95412/d8eb3/baki-manga.webp"
+            ),
+            Anime(
+                photoUrl = "https://bilder.fernsehserien.de/sendung/hr2/darling-in-the-franxx_226689.png"
             )
         )
     }
--- app/src/main/java/com/example/myanimelist/ui/WatchedScreen.kt
+++ app/src/main/java/com/example/myanimelist/ui/WatchedScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -54,7 +55,8 @@ fun WatchedScreen(navController: NavHostController) {
         )
         Column(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
