f234b457dbd0f1d8dc8f323664cca460d770b534
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-13 13:20:35 -0300

    create CustomFavorite item to show up in Favorite Screen

--- app/src/main/java/com/example/myanimelist/presentation/theme/ui/util/CustomItem.kt
+++ app/src/main/java/com/example/myanimelist/presentation/theme/ui/util/CustomItem.kt
@@ -3,18 +3,21 @@ package com.example.myanimelist.presentation.theme.ui.util
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.myanimelist.domain.model.Anime
@@ -51,6 +54,25 @@ fun CustomFavorite(anime: Anime, position: Int) {
         else -> defaultColor
     }
 
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "${anime.order}°",
+            color = textColor,
+            fontWeight = FontWeight.Light
+        )
+        Image(
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp)),
+            painter = rememberAsyncImagePainter(anime.photoUrl),
+            contentDescription = "Anime Screen",
+            contentScale = ContentScale.FillBounds,
+        )
+    }
 
-    
 }
