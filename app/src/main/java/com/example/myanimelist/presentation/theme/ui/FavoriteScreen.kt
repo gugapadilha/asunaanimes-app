785b9c8e3433bc7906e6a6a653e31f091455f4d6
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-13 14:07:26 -0300

    created favorite screen and showing the anime list in sequence

--- app/src/main/java/com/example/myanimelist/presentation/theme/ui/FavoriteScreen.kt
+++ app/src/main/java/com/example/myanimelist/presentation/theme/ui/FavoriteScreen.kt
@@ -1,11 +1,59 @@
 package com.example.myanimelist.presentation.theme.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.example.myanimelist.R
+import com.example.myanimelist.data.repository.AnimeRepository
+import com.example.myanimelist.presentation.theme.ui.util.CustomFavorite
 
 @Composable
-fun FavoriteScreen(navController : NavHostController) {
-    
-    Text(text = "Favorite Screen")
+fun FavoriteScreen(navController: NavHostController) {
+    val animeRepository = AnimeRepository()
+    val getAllAnimeData = animeRepository.getAllData()
+
+    val painter = rememberAsyncImagePainter(R.drawable.watched_screen)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painter,
+            contentDescription = "Background Image",
+            contentScale = ContentScale.FillBounds
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            item {
+                Text(
+                    text = "Top 10 Anime List:",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+
+            itemsIndexed(items = getAllAnimeData) { index, anime ->
+                CustomFavorite(anime = anime, position = index)
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/myanimelist/presentation/theme/ui/util/CustomItem.kt
+++ app/src/main/java/com/example/myanimelist/presentation/theme/ui/util/CustomItem.kt
@@ -5,10 +5,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +24,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.example.myanimelist.data.repository.AnimeRepository
 import com.example.myanimelist.domain.model.Anime
 
 @Composable
@@ -67,11 +73,27 @@ fun CustomFavorite(anime: Anime, position: Int) {
             fontWeight = FontWeight.Light
         )
         Image(
+            painter = rememberImagePainter(data = anime.photoUrl, builder = {
+            }),
+            contentDescription = "Anime picture",
             modifier = Modifier
-                .clip(RoundedCornerShape(16.dp)),
-            painter = rememberAsyncImagePainter(anime.photoUrl),
-            contentDescription = "Anime Screen",
-            contentScale = ContentScale.FillBounds,
+                .height(200.dp)
+                .fillMaxWidth()
+        )
+        Text(
+            text = anime.name,
+            color = Color.White,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(10.dp)
+            .background(Color.Black)
+        )
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(Color.White)
         )
     }
 
