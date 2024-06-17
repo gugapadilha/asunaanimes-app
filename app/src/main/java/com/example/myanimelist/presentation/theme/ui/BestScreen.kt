bf04d59359677cddf22329bbe88b62ec3162b6ff
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-17 13:16:39 -0300

    create best screen

---
+++ app/src/main/java/com/example/myanimelist/presentation/theme/ui/BestScreen.kt
@@ -0,0 +1,9 @@
+package com.example.myanimelist.presentation.theme.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun BestScreen(navController : NavHostController ) {
+
+}
