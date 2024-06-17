363b7c39b5bd11bedd04067c60519f8eb135ef4e
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-06-17 14:15:01 -0300

    create custom item to best animes

--- app/src/main/java/com/example/myanimelist/presentation/theme/ui/BestScreen.kt
+++ app/src/main/java/com/example/myanimelist/presentation/theme/ui/BestScreen.kt
@@ -1,6 +1,5 @@
 package com.example.myanimelist.presentation.theme.ui
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import com.example.myanimelist.presentation.theme.ui.util.CustomBest
