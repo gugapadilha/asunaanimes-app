96ef00ae49d04307a58593dfcd84281764d74cb4
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-07-02 17:45:08 -0300

    added gson depnedency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-
+    //Gson
+    implementation("com.google.code.gson:gson:2.10")
 
 }
