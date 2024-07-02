3f3937efa8a62acf63e2d5f18a32f5901035a74b
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-07-02 17:43:34 -0300

    added retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,10 @@ dependencies {
     //Orbital
     implementation("com.github.skydoves:orbital:0.3.2")
 
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
+
 }
