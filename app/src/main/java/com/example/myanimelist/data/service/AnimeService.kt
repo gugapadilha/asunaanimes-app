3e799b1f3749d98fa8f2150f407c548cec1e25fb
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-07-03 13:13:59 -0300

    create anime service builder retrofit

--- app/src/main/java/com/example/myanimelist/data/service/AnimeService.kt
+++ app/src/main/java/com/example/myanimelist/data/service/AnimeService.kt
@@ -1,4 +1,19 @@
 package com.example.myanimelist.data.service
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 interface AnimeService {
+
+    companion object  {
+        val BASE_URL = "https://docs.api.jikan.moe/"
+
+        fun create(): AnimeService {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+            return retrofit.create(AnimeService::class.java)
+        }
+    }
 }
