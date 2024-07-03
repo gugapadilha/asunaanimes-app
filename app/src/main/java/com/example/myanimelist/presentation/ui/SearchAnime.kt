71174d1c140d93ccd92d61a1506199b5d56e5a3c
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-07-03 14:42:22 -0300

    created search anime

---
+++ app/src/main/java/com/example/myanimelist/presentation/ui/SearchAnime.kt
@@ -0,0 +1,10 @@
+package com.example.myanimelist.presentation.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun SearchAnime(navController : NavHostController) {
+
+}
