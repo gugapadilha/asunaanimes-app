150e19c1b07b01503e4521e40b44a4d380782f44
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-07-03 13:17:48 -0300

    create both top and search animes

--- app/src/main/java/com/example/myanimelist/data/service/AnimeService.kt
+++ app/src/main/java/com/example/myanimelist/data/service/AnimeService.kt
@@ -1,10 +1,18 @@
 package com.example.myanimelist.data.service
 
+import android.telecom.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AnimeService {
 
+    @GET("top/anime")
+    fun getTopAnimes(): Call<>
+
+    @GET("search/anime")
+    fun getSearchedAnimes(@Query("q")queryString: String): Call<>
     companion object  {
         val BASE_URL = "https://docs.api.jikan.moe/"
 
