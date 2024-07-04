384ed17532be0ff040e9ef811a1c7363dd573c77
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-07-04 14:06:12 -0300

    custom search class defined

--- app/src/main/java/com/example/myanimelist/presentation/util/CustomSearch.kt
+++ app/src/main/java/com/example/myanimelist/presentation/util/CustomSearch.kt
@@ -1,8 +1,19 @@
 package com.example.myanimelist.presentation.util
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun CustomSearch() {
-    
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+    }
 }
