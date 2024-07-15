4845260fc6ba6cebcafe62c5061ce96a937dc81f
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-07-15 14:45:53 -0300

    added back image to search screen

--- app/src/main/java/com/example/myanimelist/presentation/ui/SearchScreen.kt
+++ app/src/main/java/com/example/myanimelist/presentation/ui/SearchScreen.kt
@@ -1,12 +1,17 @@
 package com.example.myanimelist.presentation.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.navigation.NavHostController
+import coil.compose.rememberAsyncImagePainter
+import com.example.myanimelist.R
 import com.example.myanimelist.data.repository.AnimeRepository
 import com.example.myanimelist.presentation.util.AnimeItem
 import com.example.myanimelist.presentation.util.SearchBox
@@ -16,17 +21,26 @@ import com.example.myanimelist.presentation.util.SearchBox
 fun SearchScreen(navController : NavHostController) {
     val animeRepository = AnimeRepository()
     val getAllAnimeData = animeRepository.getAllData()
+    val painter = rememberAsyncImagePainter(R.drawable.favorite_screen)
 
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        SearchBox()
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painter,
+            contentDescription = "Background Image",
+            contentScale = ContentScale.FillBounds
+        )
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            SearchBox()
 
-        LazyColumn {
-            items(getAllAnimeData) { anime ->
-                AnimeItem(anime = anime)
+            LazyColumn {
+                items(getAllAnimeData) { anime ->
+                    AnimeItem(anime = anime)
+                }
+
             }
-
         }
     }
 }
