b6cbc42c20cfedba2670d1802e86fe32ca76c775
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-07-24 12:59:45 -0300

    start to create bottomsheet

--- app/src/main/java/com/example/myanimelist/presentation/ui/bottomsheet/AnimeDetailsBottomSheet.kt
+++ app/src/main/java/com/example/myanimelist/presentation/ui/bottomsheet/AnimeDetailsBottomSheet.kt
@@ -1,8 +1,63 @@
 package com.example.myanimelist.presentation.ui.bottomsheet
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
+import com.example.myanimelist.domain.model2.Data
 
 @Composable
-fun AnimeDetailsBottomSheet() {
+fun AnimeDetailsBottomSheet(anime : Data) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(8.dp))
+        ) {
+            Image(
+                painter = rememberImagePainter(data = anime.images.jpg.imageUrl),
+                contentDescription = "Anime picture",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Black.copy(alpha = 0.7f))
+                    .padding(4.dp)
+                    .align(Alignment.BottomStart)
+            ) {
+                Text(
+                    text = anime.title,
+                    color = Color.White,
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily.SansSerif,
+                    textAlign = TextAlign.Start,
+                )
+            }
+        }
+    }
     
 }
