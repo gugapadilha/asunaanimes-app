117d58bbeafa9f8809586c767ef397d86cbdb0c2
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-07-24 13:25:07 -0300

    creating ui to bottomsheet

--- app/src/main/java/com/example/myanimelist/presentation/ui/bottomsheet/AnimeDetailsBottomSheet.kt
+++ app/src/main/java/com/example/myanimelist/presentation/ui/bottomsheet/AnimeDetailsBottomSheet.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,13 +17,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import com.example.myanimelist.domain.model2.Data
+import java.text.SimpleDateFormat
 
 @Composable
-fun AnimeDetailsBottomSheet(anime : Data) {
+fun AnimeDetailsBottomSheet(anime: Data) {
 
     Box(
         modifier = Modifier
@@ -40,24 +43,53 @@ fun AnimeDetailsBottomSheet(anime : Data) {
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .height(200.dp)
-                    .fillMaxWidth()
+                    .width(130.dp)
             )
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Black.copy(alpha = 0.7f))
-                    .padding(4.dp)
-                    .align(Alignment.BottomStart)
-            ) {
-                Text(
-                    text = anime.title,
-                    color = Color.White,
-                    fontSize = 14.sp,
-                    fontFamily = FontFamily.SansSerif,
-                    textAlign = TextAlign.Start,
-                )
-            }
+            Text(
+                text = anime.title,
+                color = Color.White,
+                fontSize = 14.sp,
+                fontFamily = FontFamily.SansSerif,
+                textAlign = TextAlign.Start,
+            )
+            Text(
+                text = anime.score.toString(),
+                color = Color.White,
+                fontSize = 14.sp,
+                fontFamily = FontFamily.SansSerif,
+                textAlign = TextAlign.Start,
+            )
+            Text(
+                text = anime.rating,
+                color = Color.White,
+                fontSize = 14.sp,
+                fontFamily = FontFamily.SansSerif,
+                textAlign = TextAlign.Start,
+            )
+            Text(
+                text = "${anime.episodes} episodes",
+                color = Color.White,
+                fontSize = 14.sp,
+                fontFamily = FontFamily.SansSerif,
+                textAlign = TextAlign.Start,
+            )
+            Text(
+                text = anime.synopsis,
+                color = Color.White,
+                fontSize = 14.sp,
+                fontFamily = FontFamily.SansSerif,
+                textAlign = TextAlign.Start,
+            )
         }
     }
-    
+}
+
+private fun formatDate(date: String): String {
+    return if (date.contains("-")){
+        val newDate = date.substring(0, date.lastIndexOf("-"))
+        val _date = SimpleDateFormat("yyyy-MM").parse(newDate)
+        SimpleDateFormat("MMM-yyyy").format(_date)
+    } else {
+        date
+    }
 }
