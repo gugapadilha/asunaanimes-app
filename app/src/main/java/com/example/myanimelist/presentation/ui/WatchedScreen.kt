55c6f7ccefc1a1add970b274369333282ce5b494
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-08-05 11:39:53 -0300

    changing watched screen

--- app/src/main/java/com/example/myanimelist/presentation/ui/WatchedScreen.kt
+++ app/src/main/java/com/example/myanimelist/presentation/ui/WatchedScreen.kt
@@ -12,15 +12,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -28,18 +32,17 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.myanimelist.R
 import com.example.myanimelist.data.repository.AnimeRepository
 import com.example.myanimelist.data.repository.YearRepository
+import com.example.myanimelist.data.service.AnimeService
+import com.example.myanimelist.domain.model2.Data
+import com.example.myanimelist.presentation.util.AnimeItem
 import com.example.myanimelist.presentation.util.CustomWatched
 
 @Composable
 fun WatchedScreen(navController: NavHostController) {
     val painter = rememberAsyncImagePainter(R.drawable.watched_screen)
-
-    val animeRepository = AnimeRepository()
-    val animeList = animeRepository.getAllData()
-
-    val yearRepository = YearRepository()
-    val yearList = yearRepository.getAllYears()
-
+    val animeList = remember { mutableStateListOf<Data>() }
+    val animeService = AnimeService.create()
+    val listState = rememberLazyListState()
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -51,37 +54,42 @@ fun WatchedScreen(navController: NavHostController) {
             contentDescription = "Background Image",
             contentScale = ContentScale.FillBounds
         )
+        Row(
+            modifier = Modifier
+                .height(100.dp)
+                .fillMaxWidth()
+                .background(Color.Red),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        )
+        {
+            Text(
+                text = "Here you can see all watched animes",
+                textAlign = TextAlign.Center,
+                color = Color.White
+            )
+        }
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
+                .fillMaxSize(),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            yearList.forEach { year ->
-                Text(
-                    text = year.year.toString(),
-                    color = Color.White,
-                    fontFamily = FontFamily.SansSerif,
-                    fontSize = 28.sp,
-                    modifier = Modifier.padding(top = 20.dp)
-                )
-                Row(
-                    modifier = Modifier.horizontalScroll(rememberScrollState()),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    animeList.forEach { anime ->
-                        CustomWatched(anime = anime)
-                    }
+            Row(
+                modifier = Modifier.horizontalScroll(rememberScrollState()),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                animeList.forEach { anime ->
+                    AnimeItem(anime = anime)
                 }
-                Spacer(
-                    modifier = Modifier
-                        .padding(top = 20.dp)
-                        .fillMaxWidth()
-                        .height(2.dp)
-                        .background(Color.Gray)
-                )
             }
+            Spacer(
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .fillMaxWidth()
+                    .height(2.dp)
+                    .background(Color.Gray)
+            )
         }
     }
 }
