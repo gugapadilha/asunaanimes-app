8f03000fe12835f557ade5134dce108effc42c79
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-08-14 16:24:07 -0300

    added the asuna animes text in home screen

--- app/src/main/java/com/example/myanimelist/presentation/ui/HomeScreen.kt
+++ app/src/main/java/com/example/myanimelist/presentation/ui/HomeScreen.kt
@@ -2,7 +2,9 @@ package com.example.myanimelist.presentation.ui
 
 import android.graphics.Color.rgb
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,6 +44,27 @@ fun HomeScreen(navController: NavController) {
             contentDescription = "Anime Screen",
             contentScale = ContentScale.FillBounds
         )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(0.dp, 100.dp, 0.dp, 0.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "ASUNA ANIMES",
+                color = Color.White,
+                fontFamily = FontFamily.Monospace,
+                fontSize = 20.sp
+            )
+            Spacer(
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .width(250.dp)
+                    .height(1.dp)
+                    .background(Color.White)
+            )
+        }
     }
 
     Column(
